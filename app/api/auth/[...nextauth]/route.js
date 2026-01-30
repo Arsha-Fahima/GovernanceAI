c88ff8e01815ed909bd234a3b864737ad0c6690b
Arsha Fahima <arsha.tajdeen23@gmail.com>
2026-01-30 14:43:29 +0530

    Change maybeSingle to single for user query

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -19,7 +19,7 @@ const handler = NextAuth({
           .from("users")
           .select("id, email")
           .eq("email", email)
-          .maybeSingle();
+          .single();
 
         // Only insert if user doesn't exist
         if (!existingUser) {
