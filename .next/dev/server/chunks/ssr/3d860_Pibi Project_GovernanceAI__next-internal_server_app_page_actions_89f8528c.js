1b5d066db49ee4d5b7ae4d1ea6454a9bb3f48cd5
Arsha-Fahima <arsha.tajdeen23@gmail.com>
2026-03-03 12:31:46 +0530

    update

--- .next/dev/server/chunks/ssr/3d860_Pibi Project_GovernanceAI__next-internal_server_app_page_actions_89f8528c.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/Desktop/Pibi Project/GovernanceAI/.next-internal/server/app/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=3d860_Pibi%20Project_GovernanceAI__next-internal_server_app_page_actions_89f8528c.js.map
--- .next/dev/static/chunks/177c1_next_dist_client_components_builtin_global-error_88b21412.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/177c1_next_dist_faf1f15e._.js"
-],
-    source: "dynamic"
-});
